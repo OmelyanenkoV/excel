d09d2b55fcef694f44be7f77746dc86e99192bc1
Vlad Omelyanenko <47278133+worldofakk@users.noreply.github.com>
2020-11-10 12:12:20 +0300

    optimization perfomance

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,25 +19,41 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]');
       const type = $resizer.data.resize;
       const coords = $parent.getCoords();
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-      $resizer.updateCss({ background: '#3c74ff', height: '100vh' });
+      const sideProp = type === 'col' ? 'bottom' : 'right';
+      let value;
+
+      $resizer.updateCss({ background: '#3c74ff', [sideProp]: '-5000px' });
+
       document.onmousemove = (e) => {
         if (type === 'col') {
           const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
-          $parent.updateCss({ widht: value + 'px' });
-          cells.forEach((el) => (el.style.width = value + 'px'));
+          value = coords.width + delta;
+          $resizer.updateCss({ right: -delta + 'px' });
         } else {
           const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
-          $parent.updateCss({ height: value + 'px' });
+          value = coords.height + delta;
+          $resizer.updateCss({ bottom: -delta + 'px' });
         }
       };
 
       document.onmouseup = () => {
         document.onmousemove = null;
         document.onmouseup = null;
-        $resizer.updateCss({ background: 'transparent' });
+
+        if (type === 'col') {
+          $parent.updateCss({ widht: value + 'px' });
+          this.$root
+            .findAll(`[data-col="${$parent.data.col}"]`)
+            .forEach((el) => (el.style.width = value + 'px'));
+        } else {
+          $parent.updateCss({ height: value + 'px' });
+        }
+
+        $resizer.updateCss({
+          background: 'none',
+          bottom: 0,
+          right: 0,
+        });
       };
     }
   }
