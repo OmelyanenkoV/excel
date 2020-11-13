65c7bc598633677df16dfff9878fa130881a9665
Vlad Omelyanenko <47278133+worldofakk@users.noreply.github.com>
2020-11-13 11:28:44 +0300

    added select group of elements

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import { $ } from '@core/Dom';
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
-import { isCell, shouldResize } from './table.functions';
+import { isCell, shouldResize, matrix } from './table.functions';
 import { TableSelection } from './TableSelection';
 
 export class Table extends ExcelComponent {
@@ -32,7 +32,14 @@ export class Table extends ExcelComponent {
       resizeHandler(this.$root, event);
     } else if (isCell(event)) {
       const $target = $(event.target);
-      this.selection.select($target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        );
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 }
